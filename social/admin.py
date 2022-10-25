8288fc87fae6ba21159b758b03a4b186efcfe930
Tilio77 <tiliobolso1@gmail.com>
2022-10-25 14:57:34 -0300

    Upload posts

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import SocialPost, SocialComment, Image
 
 # Register your models here.
+
+admin.site.register(SocialComment)
+admin.site.register(SocialPost)
+admin.site.register(Image)
